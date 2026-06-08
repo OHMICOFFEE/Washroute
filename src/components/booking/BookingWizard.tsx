8fdb8ca0e2e1c84f4a2b0f68999f2b9298bc8ae1
washroute <platinumswealth@gmail.com>
2026-06-08 09:29:28 +0200

    Delete src directory

--- src/components/booking/BookingWizard.tsx
+++
@@ -1,5 +0,0 @@
-// BookingWizard is replaced by StaticBookingWizard in demo mode
-// This file is kept as a placeholder to avoid import errors
-export default function BookingWizard() {
-  return null
-}
